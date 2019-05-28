0b77c5047b9a959aad927432d838457937fb0e86
xitiomet <brian@openstatic.org>
2019-05-28 12:54:47 -0400

    updated version number

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ export class FirebaseAuth {
 
     function validateAuthData(authData, options)
     {
-        return options.admin.auth().verifyIdToken(authData.access_token).then(function (decodedToken)
-        {
-            console.log(JSON.stringify(decodedToken));
-            if (decodedToken && decodedToken.uid == authData.id)
+        return options.admin.auth().verifyIdToken(authData.access_token).then(
+            function (decodedToken)
             {
-                return;
-            }
-            throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Firebase auth not found for this user.');
+                console.log(JSON.stringify(decodedToken));
+                if (decodedToken && decodedToken.uid == authData.id)
+                {
+                    return;
+                }
+                throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Firebase auth not found for this user.');
 
-        }).catch(function (error) {
+            }
+        ).catch(function (error) {
             console.log(error);
             throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Firebase auth is invalid for this user.');
         });
