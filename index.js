32acb7523c03933892d4a82ae3b836d688d261ae
xitiomet <brian@openstatic.org>
2019-05-28 13:14:31 -0400

    rolling back

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-class FirebaseAuth
+function validateAuthData(authData, options)
 {
-    function validateAuthData(authData, options)
+    return options.admin.auth().verifyIdToken(authData.access_token).then(function (decodedToken)
     {
-        return options.admin.auth().verifyIdToken(authData.access_token).then(
-            function (decodedToken)
-            {
-                console.log(JSON.stringify(decodedToken));
-                if (decodedToken && decodedToken.uid == authData.id)
-                {
-                    return;
-                }
-                throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Firebase auth not found for this user.');
+        console.log(JSON.stringify(decodedToken));
+        if (decodedToken && decodedToken.uid == authData.id)
+        {
+            return;
+        }
+        throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Firebase auth not found for this user.');
 
-            }
-        ).catch(function (error) {
-            console.log(error);
-            throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Firebase auth is invalid for this user.');
-        });
-    }
-
-    function validateAppId(appIds, authData) {
-        return Promise.resolve();
-    }
+    }).catch(function (error) {
+        console.log(error);
+        throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Firebase auth is invalid for this user.');
+    });
 }
 
-export default FirebaseAuth;
-module.exports = FirebaseAuth;
+function validateAppId(appIds, authData) {
+    return Promise.resolve();
+}
+
+module.exports = {
+  validateAppId: validateAppId,
+  validateAuthData: validateAuthData
+};
